aa764f492d5019c25b8ed11e72b864039876229e
GuvidSlayer <andgeorge0015@gmail.com>
2024-04-12 20:08:00 +0300

    a

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const instance = basicLightbox.create(`
+	<h1>Dynamic Content</h1>
+	<p>You can set the content of the lightbox with JS.</p>
+`);
+
+instance.show();
+
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+});
 console.log(galleryItems);
