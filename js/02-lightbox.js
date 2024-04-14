e4df879f7fe5bfb0dbeec677999e5694bda7beff
GuvidSlayer <andgeorge0015@gmail.com>
2024-04-14 15:32:53 +0300

    nbm h

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
 const gallery = document.querySelector(".gallery");
 
 const fullGallery = galleryItems
@@ -20,4 +21,3 @@ let lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
 });
-console.log(galleryItems);
